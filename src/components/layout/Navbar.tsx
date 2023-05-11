815c0bf729e8537e3f2c6e5993cbb0d8197a191d
2023LEAP3-Turuu <turuu@NEST-96s-iMac.local>
2023-05-11 15:08:18 +0800

    new design doing

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,33 +3,44 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <div className="w-full h-[100px] flex justify-center bg-[#fff]">
-      <div className="h-full max-w-[1920px] w-full px-[60px] flex flex-row justify-between items-center">
-        <h1 className="text-[18px] font-['Poppins'] font-medium">UB Events</h1>
-        <div className="flex flex-row gap-[100px]">
-          <div className="flex gap-[24px] items-center">
-            {["Home", "Favorites", "Calendar"].map((el, i) => {
+    <div className="w-full h-[80px] fixed">
+      <div className="h-full max-w-[1920px] w-full px-[60px] m-auto flex flex-row justify-between items-center text-[#fff]">
+        {/* left logo */}
+        <button
+          onClick={() => {
+            console.log("hello");
+          }}
+        >
+          <h1 className="text-[24px] font-['Roboto']">UB EVENTS</h1>
+        </button>
+        {/* right profile avatar */}
+        <button className="flex items-center gap-[10px] justify-center">
+          <Image
+            src="https://images.unsplash.com/photo-1682424609336-9f71d1df4f30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2487&q=80"
+            width={40}
+            height={40}
+            alt="user avatar"
+            className="rounded-[50%] w-[40px] h-[40px]"
+          />
+          <h2 className="font-['Inter'] font-normal pl-[5px]">Doljinsuren</h2>
+          <Image src="/otherIcons/navbar-arrowDown.svg" width={30} height={30} alt="user avatar" className="w-[30px] h-[30px]" />
+          <div className="flex flex-col mt-[200px] w-[245px] p-[20px] bg-black">
+            {["Хадгалсан", "Тасалбар", "Календар", "Тохиргоо"].map((el, i) => {
               return (
-                <button key={i} onClick={() => console.log(el)} className="h-[30px]">
-                  <h2 className="text-[18px] font-['Poppins'] font-normal">{el}</h2>
-                </button>
+                <div key={i} className="flex flex-row h-[45px] items-center px-[10px] justify-between">
+                  <h2>{el}</h2>
+                  <Image
+                    src="/otherIcons/navbar-arrowDown.svg"
+                    width={30}
+                    height={30}
+                    alt="user avatar"
+                    className="w-[30px] h-[30px] rotate-[-90deg]"
+                  />
+                </div>
               );
             })}
           </div>
-          <div className="w-[300px] h-[70px] flex justify-between items-center">
-            <div className="pl-[30px]">
-              <Image
-                src="https://images.unsplash.com/photo-1682424609336-9f71d1df4f30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2487&q=80"
-                width={50}
-                height={50}
-                alt="user avatar"
-                className="rounded-[50%] max-h-[50px] max-w-[50px] min-h-[50px] min-w-[50px]"
-              />
-            </div>
-            {/* user email */}
-            <h2 className="text-[18px] font-['Poppins'] font-normal pl-[10px]">surnee@gmail.com</h2>
-          </div>
-        </div>
+        </button>
       </div>
     </div>
   );
